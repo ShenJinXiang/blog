40de5fc21aaa39042eadb85d127b59aa3a2c1908
shenjinxiang <sjx-sword@163.com>
2016-07-12 23:39:20 +0800

    some js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var LS = (function() {
 		url : 'http://shenjinxiang.com',
 		//ctx : "/",
 		//url : 'http://shenjinxiang.github.io',
-		ctx : "/blog/",
-		//ctx : "/Users/shenjinxiang/Documents/blog/",
+		//ctx : "/blog/",
+		ctx : "/Users/shenjinxiang/Documents/blog/",
 	    logo : {
 			'title' : 'LikeStar',
 			'url' : 'index.html'
