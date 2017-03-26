59412cba7398218650b4e0e466937965624d0a68
shenjinxiang <sjx-sword@163.com>
2017-03-26 23:32:39 +0800

    markcanvas

--- pages/mydemo/canvas/isPointInPath/index.js
+++ pages/mydemo/canvas/isPointInPath/index.js
@@ -1,11 +1,12 @@
 (function (){
-	let canvas = document.getElementById('canvas');
+	var canvas = document.getElementById('canvas');
+	var markCanvas = getMarkCanvas();
 	canvas.width = window.innerWidth;
 	canvas.height = window. innerHeight;
-	let context = canvas.getContext('2d');
+	var context = canvas.getContext('2d');
 
 	// 配置信息
-	let config = {
+	var config = {
 		num: 20,
 		minr: 10,
 		maxr: 40,
@@ -13,14 +14,14 @@
 		ccolor: 'red'
 	};
 
-	let balls = [];
-	let box = canvas.getBoundingClientRect();
+	var balls = [];
+	var box = canvas.getBoundingClientRect();
 
 	init();
 
 	// 初始化
 	function init() {
-		for (let i = 0; i < config.num; i++) {
+		for (var i = 0; i < config.num; i++) {
 			balls[i] = {
 				x: Math.random() * canvas.width,
 				y: Math.random() * canvas.height,
@@ -40,12 +41,12 @@
 
 	function draw(e) {
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		let x = -1000, y = -1000;
+		var x = -1000, y = -1000;
 		if (e) {
 			x = e.clientX - box.left;
 			y = e.clientY - box.top;
 		}
-		for (let i = 0; i < balls.length; i++) {
+		for (var i = 0; i < balls.length; i++) {
 			context.beginPath();
 			context.arc(balls[i].x, balls[i].y, balls[i].r, 0, 2 * Math.PI, false);
 			if (context.isPointInPath(x, y)) {
@@ -56,6 +57,21 @@
 			context.fill();
 			context.closePath();
 		}
+		context.drawImage(markCanvas, canvas.width - markCanvas.width, canvas.height - markCanvas.height);
+	}
+
+	function getMarkCanvas() {
+		var markCanvas = document.createElement('canvas');
+		markCanvas.width = 400;
+		markCanvas.height = 100;
+		var ctx = markCanvas.getContext('2d');
+		
+		ctx.fillStyle = 'rgba(204, 204, 204, 0.5)';
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.font = '40px cursive';
+		ctx.fillText('www.shenjinxiang.com', markCanvas.width / 2, markCanvas.height / 2, 340);
+		return markCanvas;
 	}
 })();
 
