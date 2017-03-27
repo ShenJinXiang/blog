80d39ac97fe83521fce675bd987025ba3ebae4ef
shenjinxiang <sjx-sword@163.com>
2017-03-27 13:00:32 +0800

    zoom

---
+++ pages/mydemo/canvas/zoom/zoom.js
@@ -0,0 +1,51 @@
+(function(){
+
+	var config = {
+		img: '../magnifier/001.jpg',
+		min: 0.5,
+		max: 3
+	};
+	var canvas = document.getElementById('canvas');
+	canvas.width = window.innerWidth - 200;
+	canvas.height = window.innerHeight - 100;
+	var context = canvas.getContext('2d');
+
+	var markCanvas = getMarkCanvas();
+
+	var range = document.getElementById('range');
+	range.style.width = canvas.width + 'px';
+	range.max = config.max;
+	range.min = config.min;
+	range.step = 0.01;
+
+	var img = new Image();
+	img.src = config.img;
+	img.onload = function () {
+		draw(1);
+	}
+
+	range.addEventListener('mousemove', function() {
+		var scale = range.value;
+		draw(scale);
+	});
+
+	function draw(scale) {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.drawImage(img, (canvas.width - canvas.width * scale) / 2, (canvas.height - canvas.height * scale) / 2, scale * canvas.width, scale * canvas.height);
+		context.drawImage(markCanvas, canvas.width - markCanvas.width, canvas.height - markCanvas.height);
+	}
+	
+	function getMarkCanvas() {
+		var markCanvas = document.createElement('canvas');
+		markCanvas.width = 400;
+		markCanvas.height = 100;
+		var ctx = markCanvas.getContext('2d');
+		
+		ctx.fillStyle = 'rgba(204, 204, 204, 0.5)';
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.font = '40px cursive';
+		ctx.fillText('www.shenjinxiang.com', markCanvas.width / 2, markCanvas.height / 2, 340);
+		return markCanvas;
+	}
+})();
