466218702142241d03b7bdba82ad0f56b4ebec4b
shenjinxiang <sjx-sword@163.com>
2017-04-19 13:12:48 +0800

    demo

---
+++ static/canvas/2017/03/14/002.js
@@ -0,0 +1,41 @@
+(function() {
+	var canvas = document.getElementById('mycanvas');
+	canvas.width = 400;
+	canvas.height = 200;
+	var context = canvas.getContext('2d');
+
+	context.lineWidth = 3;
+	context.fillStyle = '#058';
+
+	context.beginPath();
+	context.arc(50, 50, 35, 0, 0.5 * Math.PI, false);
+	context.stroke();
+
+	context.beginPath();
+	context.arc(150, 50, 35, 0, 1 * Math.PI, false);
+	context.stroke();
+
+	context.beginPath();
+	context.arc(250, 50, 35, 0, 1.5 * Math.PI, false);
+	context.stroke();
+
+	context.beginPath();
+	context.arc(350, 50, 35, 0, 2 * Math.PI, false);
+	context.stroke();
+
+	context.beginPath();
+	context.arc(50, 150, 35, 0, 0.5 * Math.PI, false);
+	context.fill();
+
+	context.beginPath();
+	context.arc(150, 150, 35, 0, 1 * Math.PI, false);
+	context.fill();
+
+	context.beginPath();
+	context.arc(250, 150, 35, 0, 1.5 * Math.PI, false);
+	context.fill();
+
+	context.beginPath();
+	context.arc(350, 150, 35, 0, 2 * Math.PI, false);
+	context.fill();
+})();
